c7820dd6fe93cf4b08e205d12ef7151ae9613f03
Yashwanth20050906 <145742983+Yashwanth20050906@users.noreply.github.com>
2026-09-19 16:46:16 +0530

    Add files via upload

---
+++ run/aes_filelist.f
@@ -0,0 +1,18 @@
+// =============================================================
+// filelist.f
+// VCS filelist for AES-256 AXI project
+// Run from: aes256_axi/run/
+// =============================================================
+
+// RTL files
+../rtl/verilog_aes/aes_sbox.v
+../rtl/verilog_aes/aes_inv_sbox.v
+../rtl/verilog_aes/aes_rcon.v
+../rtl/verilog_aes/aes_key_expand_256.v
+../rtl/verilog_aes/aes_cipher_top.v
+../rtl/verilog_aes/aes_inv_cipher_top.v
+../rtl/aes_axi_slave/aes_axi_slave.sv
+
+// Testbench files
+../tb/aes_axi_slave_tb.sv
+../tb/aes_test_bench_top.v
---
+++ run/uart_filelist.f
@@ -0,0 +1,10 @@
++incdir+../rtl/uart/include
+
+../rtl/uart/axi_internal_fifo.v
+../rtl/uart/uart_parity_bit_compute.v
+../rtl/uart/uart_receiver.v
+../rtl/uart/uart_transmitter.v
+../rtl/uart/uart_controller.v
+../rtl/uart/axi_uart_top.v
+
+../tb/uart_standalone_tb.sv
